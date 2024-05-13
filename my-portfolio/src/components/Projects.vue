71d86410db79f3d4122e1f011913a23aef93de4f
agusvlopez <agusvlopez@gmail.com>
2024-05-13 12:13:08 -0300

    add: projects

--- my-portfolio/src/components/Projects.vue
+++ my-portfolio/src/components/Projects.vue
@@ -4,6 +4,8 @@ import trainingApp from '../assets/trainingapp-laptop.jpg';
 import traniCeramica from '../assets/traniceramica-laptop.jpg';
 import apiWeather from '../assets/apiweather-mobile.jpg';
 import recipesBySam from '../assets/recipes-by-sam-laptop.jpg';
+import fanWeddingHeineken from '../assets/fan-wedding-heineken.png';
+import plusbelle from '../assets/plusbelle.png';
 import Navbar from './Navbar.vue';
 
 </script>
@@ -24,15 +26,56 @@ import Navbar from './Navbar.vue';
             </div>
         </div>
     </section>
-    <div id="allProjects" class="min-h-screen flex items-center justify-center ">
+
+    <div id="allProjects" class="min-h-screen flex column items-center justify-center">
+
         <div class="grid grid-cols-2 grid-rows-2 gap-2 mt-2 mb-2">
+            <div class="flex items-center justify-center relative group">
+                <a href="https://www.heineken.com/ar/es/fanwedding">
+                    <img :src="fanWeddingHeineken" alt="Fan Wedding - Heineken"
+                        class="w-full h-full object-cover opacity-85 group-hover:opacity-100 transition-opacity duration-300" />
+                    <div
+                        class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-80 opacity-0 group-hover:opacity-100 transition-opacity duration-300 project--item">
+                        <div>
+                            <p class="text-white text-lg text-center font-semibold justify-center">PROFESSIONAL
+                            </p>
+                            <h2 class="text-white text-3xl font-bold uppercase text-center">Fan Wedding - Heineken</h2>
+                            <p class="text-white text-lg text-center justify-center mb-2">Mails, forms and database
+                                management.
+                            </p>
+                            <p class="text-white text-lg text-center justify-center">MySQL - Laravel
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="flex items-center justify-center relative group">
+                <a href="http://plusbelle.com.ar/">
+                    <img :src="plusbelle" alt="Plusbelle"
+                        class="w-full h-full object-cover opacity-85 group-hover:opacity-100 transition-opacity duration-300" />
+                    <div
+                        class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-80 opacity-0 group-hover:opacity-100 transition-opacity duration-300 project--item">
+                        <div>
+                            <p class="text-white text-lg text-center font-semibold justify-center">PROFESSIONAL
+                            </p>
+                            <h2 class="text-white text-3xl font-bold uppercase text-center">Plusbelle</h2>
+                            <p class="text-white text-lg text-center justify-center mb-2">Front End development.
+                            </p>
+                            <p class="text-white text-lg text-center justify-center">React.js - Tailwindcss - SASS
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
             <div class="flex items-center justify-center relative group">
                 <a href="https://training-app-orpin.vercel.app/#/">
                     <img :src="trainingApp" alt="Laptop with Training App page"
                         class="w-full h-full object-cover opacity-85 group-hover:opacity-100 transition-opacity duration-300" />
                     <div
-                        class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-70 opacity-0 group-hover:opacity-100 transition-opacity duration-300 project--item">
+                        class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-80 opacity-0 group-hover:opacity-100 transition-opacity duration-300 project--item">
                         <div>
+                            <p class="text-white text-xl text-center font-semibold justify-center">PERSONAL
+                            </p>
                             <h2 class="text-white text-3xl font-bold uppercase text-center">Training App</h2>
                             <p class="text-white text-lg text-center justify-center">Vue.js 3 - Firebase - Tailwindcss
                             </p>
@@ -45,8 +88,10 @@ import Navbar from './Navbar.vue';
                     <img :src="traniCeramica" alt="Laptop with Trani Cerámica page"
                         class="w-full h-full object-cover opacity-85 group-hover:opacity-100 transition-opacity duration-300" />
                     <div
-                        class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-70 opacity-0 group-hover:opacity-100 transition-opacity duration-300 project--item">
+                        class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-80 opacity-0 group-hover:opacity-100 transition-opacity duration-300 project--item">
                         <div>
+                            <p class="text-white text-xl text-center font-semibold justify-center">PERSONAL
+                            </p>
                             <h2 class="text-white text-3xl font-bold uppercase text-center">Trani Cerámica</h2>
                             <p class="text-white text-lg text-center">PHP - Bootstrap - MySQL</p>
                         </div>
@@ -58,8 +103,10 @@ import Navbar from './Navbar.vue';
                     <img :src="apiWeather" alt="Mobile with Api Weather page"
                         class="w-full h-full object-cover opacity-85 group-hover:opacity-100 transition-opacity duration-300" />
                     <div
-                        class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-70 opacity-0 group-hover:opacity-100 transition-opacity duration-300 project--item">
+                        class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-80 opacity-0 group-hover:opacity-100 transition-opacity duration-300 project--item">
                         <div>
+                            <p class="text-white text-xl text-center font-semibold justify-center">PERSONAL
+                            </p>
                             <h2 class="text-white text-3xl font-bold uppercase text-center">API Weather</h2>
                             <p class="text-white text-lg text-center">Javascript - HTML - CSS</p>
                         </div>
@@ -71,8 +118,10 @@ import Navbar from './Navbar.vue';
                     <img :src="recipesBySam" alt="Laptop with 'Recipes by Sam' page"
                         class="w-full h-full object-cover opacity-85 group-hover:opacity-100 transition-opacity duration-300" />
                     <div
-                        class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-70 opacity-0 group-hover:opacity-100 transition-opacity duration-300 project--item">
+                        class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-80 opacity-0 group-hover:opacity-100 transition-opacity duration-300 project--item">
                         <div>
+                            <p class="text-white text-xl text-center font-semibold justify-center">PERSONAL
+                            </p>
                             <h2 class="text-white text-3xl font-bold uppercase text-center">Recipes by Sam</h2>
                             <p class="text-white text-lg text-center">React.js - Node.js - MongoDB - Firebase -
                                 Tailwindcss
