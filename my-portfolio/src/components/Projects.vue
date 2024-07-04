5b871d76d68157d69adea1dd230b986dfc853704
agusvlopez <agusvlopez@gmail.com>
2024-07-04 11:09:55 -0300

    add: oreo and zorro projects

--- my-portfolio/src/components/Projects.vue
+++ my-portfolio/src/components/Projects.vue
@@ -6,6 +6,8 @@ import apiWeather from '../assets/apiweather-mobile.jpg';
 import recipesBySam from '../assets/recipes-by-sam-laptop.jpg';
 import fanWeddingHeineken from '../assets/fan-wedding-heineken.png';
 import plusbelle from '../assets/plusbelle.png';
+import zorro from '../assets/zorro.png';
+import oreo from '../assets/oreo.png';
 import Navbar from './Navbar.vue';
 
 </script>
@@ -30,6 +32,43 @@ import Navbar from './Navbar.vue';
     <div id="allProjects" class="min-h-screen flex column items-center justify-center">
 
         <div class="lg:grid lg:grid-cols-2 lg:grid-rows-2 gap-2 mt-2 mb-2">
+            <div class="flex items-center justify-center relative group">
+                <a href="https://www.zorro.com.ar" target="_blank">
+                    <img :src="zorro" alt="Zorro website"
+                        class="w-full h-full object-cover opacity-85 group-hover:opacity-100 transition-opacity duration-300" />
+                    <div
+                        class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-80 opacity-80 md:opacity-0 lg:group-hover:opacity-100 lg:transition-opacity lg:duration-300 project--item">
+                        <div>
+                            <p class="text-white text-lg text-center font-semibold justify-center">PROFESSIONAL
+                            </p>
+                            <h2 class="text-white text-3xl font-bold uppercase text-center">Zorro</h2>
+                            <p class="text-white text-lg text-center justify-center mb-2">Front End development.
+                            </p>
+                            <p class="text-white text-lg text-center justify-center">React.js - Tailwindcss - SASS
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="flex items-center justify-center relative group">
+                <a href="http://oreo.com.ar/" target="_blank">
+                    <img :src="oreo" alt="Oreo website"
+                        class="w-full h-full object-cover opacity-85 group-hover:opacity-100 transition-opacity duration-300" />
+                    <div
+                        class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-80 opacity-80 md:opacity-0 lg:group-hover:opacity-100 lg:transition-opacity lg:duration-300 project--item">
+                        <div>
+                            <p class="text-white text-lg text-center font-semibold justify-center">PROFESSIONAL
+                            </p>
+                            <h2 class="text-white text-3xl font-bold uppercase text-center">Oreo</h2>
+                            <p class="text-white text-lg text-center justify-center mb-2">Front End development.
+                            </p>
+                            <p class="text-white text-lg text-center justify-center">React.js - Tailwindcss
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
             <div class="flex items-center justify-center relative group">
                 <a href="https://www.heineken.com/ar/es/fanwedding" target="_blank">
                     <img :src="fanWeddingHeineken" alt="Fan Wedding - Heineken"
